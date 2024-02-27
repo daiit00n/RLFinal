c9f1968e2509042a279816c206b6e31897ff1ee0
daiit00n <84034208+daiit00n@users.noreply.github.com>
2024-02-28 03:26:43 +0600

    Update server.js

--- rlfinal/server.js
+++ rlfinal/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 
 mongoose.connect('mongodb+srv://mcswordyt:1234@cluster0.vgkl87j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB Atlas'))
